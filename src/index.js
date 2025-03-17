5d3a3be23cfbbb7799ab71e030d0bddcca20de2e
Jesús González Álvarez <Jesus.Gonzalez@unit4.com>
2025-03-17 09:00:52 +0100

    Add all imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { CounterApp } from './CounterReduxAsync'
+import { CounterApp } from './Counter'
+// import { CounterApp } from './CounterReducer'
+// import { CounterApp } from './CounterReducerProps'
+// import { CounterApp } from './CounterReducerContext'
+// import { CounterApp } from './CounterRedux'
+// import { CounterApp } from './CounterReduxAsync'
 
 document.body.innerHTML = '<div id="app"></div>';
 const root = createRoot(document.getElementById('app'));
